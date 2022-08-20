f60bc223bea95aa52277ac846aef1e58fb12cd18
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-08-20 06:06:38 +0100

    add schedule

---
+++ schedule.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# regular retrival of images
+#
+# config
+
+dst="/data/share/phone/picture-frame/"
+server="https://pix.toot.wales"
+user="ammanvms"
+secret="secret.txt"
+
+
+# constants
+log="$0.log"
+
+( (
+    cd "`dirname \"$0\"`"
+
+    source "ENV/bin/activate"
+
+    if ! [ -d "$dst" ]; then
+      echo "$dst does not exist"
+      exit
+    fi
+
+    python3 ./download.py "$dst" "$server" "$user" "$decret"
+  )
+  echo "Exit status $?"
+
+) 2>&1 | tee "$log"
